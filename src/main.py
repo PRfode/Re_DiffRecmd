96f66b2c0729390a565039b15bae5042acc1e6f8
PRfode <144321210+PRfode@users.noreply.github.com>
2025-12-11 17:14:38 +0800

    make some edits

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ from models import DiffRec_beta
 from models import DiffRec_c
 
 def parse_global_args(parser):
-	parser.add_argument('--gpu', type=str, default='0',
+	parser.add_argument('--gpu', type=str, default='',
 						help='Set CUDA_VISIBLE_DEVICES, default for CPU only')
 	parser.add_argument('--verbose', type=int, default=logging.INFO,
 						help='Logging Level, 0, 10, ..., 50')
